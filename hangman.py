b6abc58fe83bc5f36a05184a2db573bca72fdfbb
roangira99 <angirarobert7@gmail.com>
2023-02-23 17:25:14 +0300

    Set the number of lives a user has in the game

--- hangman.py
+++ hangman.py
@@ -18,11 +18,13 @@ def hangman():
     alphapet = set(string.ascii_uppercase)
     used_letters = set() # what the user has guessed
 
+    lives = 6
+
     # getting user input
-    while len(word_letters) > 0:
+    while len(word_letters) > 0 and lives > 0:
         # letters used
         # ' '.join(['a', 'b', 'cd']) --> 'a b cd'
-        print('You have used these letters: ', ' '.join(used_letters))
+        print('You have', lives, 'lives left and you have used these letters: ', ' '.join(used_letters))
 
         # what current word is (i.e W - R D) 
         word_list = [letter if letter in used_letters else '-' for letter in word]
@@ -33,13 +35,20 @@ def hangman():
             used_letters.add(user_letter) # add the letter to the used_letters set
             if user_letter in word_letters: # If the letter a user guesses is in the word, remove the letter from word_letters
                 word_letters.remove(user_letter)
+            else:
+                lives = lives - 1 # takes away a life if wrong
+                print('Letter is not in word.')
 
         elif user_letter in used_letters: # If letter entered by user is already in used_letters...
             print('You have already used that character. Please try again.')
         else: # If letter typed by a user is not in the alphabet and is not in used_letters
             print('Invalid character. Please try again')
 
-    # gets here when len(word_letters) == 0
+    # gets here when len(word_letters) == 0 or when lives == 0
+    if lives == 0:
+        print("You died, sorry. The word was", word)
+    else:
+        print("Congratulations! you guessed the word", word, "!!")
 
 hangman()
 # user_input = input('Type something: ')
